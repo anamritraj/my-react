cd754a21d85af83110e653ba2c57d8aa67b34c61
anamritraj <anandamritraj904@gmail.com>
2019-01-22 10:45:18 +0530

    Update the demo to use jsx

---
+++ demo/js/dist/bundle.js
@@ -0,0 +1,83 @@
+"use strict";
+
+/** @jsx MyReact.createElement */
+var MyReact = getMyReact();
+var MyTodos = ["Implement React render", "Implement React component", "Implement React fiber", "Replace original React", "Conquer the world!"];
+var TEXT_ELEMENT = "TEXT_ELEMENT";
+var myElement = MyReact.createElement("div", null, MyReact.createElement("ul", null, MyReact.createElement("li", null, "Implement React render"), MyReact.createElement("li", null, "Implement React component"), MyReact.createElement("li", null, "Implement React fiber"), MyReact.createElement("li", null, "Replace original React"), MyReact.createElement("li", null, "Conquer the world!")));
+MyReact.render(myElement, document.getElementById("my-react-app")); // Probably need to build everything into a single file.
+// For now just adding the source code of MyReact here since require() is not supported ny browsers
+
+function getMyReact() {
+  function render(element, parent) {
+    // Get the props and type from element object
+    var type = element.type,
+        props = element.props; // Check if it is a text element
+
+    var isTextElement = type === "TEXT_ELEMENT"; // Create a new dom element
+
+    var dom = !isTextElement ? document.createElement(type) : document.createTextNode(props.nodeValue); // Filter for eventListeners in the props
+
+    var isListener = function isListener(name) {
+      return name.startsWith("on");
+    }; // Add eventListeners to the dom element
+
+
+    Object.keys(props).filter(isListener).forEach(function (name) {
+      var eventType = name.toLowerCase().substring(2);
+      dom.addEventListener(eventType, props[name]);
+    });
+
+    var isAttribute = function isAttribute(name) {
+      return !isAttribute && name !== "children";
+    };
+
+    Object.keys(props).filter(isAttribute).forEach(function (name) {
+      dom[name] = props[name];
+    }); // Check if there are any childrens of the given element
+
+    var childElements = props.children || []; // render those childrens recursively first
+
+    childElements.forEach(function (el) {
+      return render(el, dom);
+    }); // finally append the element to the parent element
+
+    parent.appendChild(dom);
+  }
+
+  function createElement(type, config) {
+    var _ref;
+
+    var props = Object.assign({}, config);
+
+    for (var _len = arguments.length, args = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+      args[_key - 2] = arguments[_key];
+    }
+
+    var hasChildren = args.length > 0;
+    var allChildren = hasChildren ? (_ref = []).concat.apply(_ref, args) : [];
+    props.children = allChildren.filter(function (child) {
+      return child !== null && child !== false;
+    }).map(function (child) {
+      return child instanceof Object ? child : createTextElement(child);
+    });
+    return {
+      type: type,
+      props: props
+    };
+  }
+
+  function createTextElement(value) {
+    return {
+      type: TEXT_ELEMENT,
+      props: {
+        nodeValue: value
+      }
+    };
+  }
+
+  return {
+    render: render,
+    createElement: createElement
+  };
+}
---
+++ demo/js/src/main.js
@@ -0,0 +1,99 @@
+/** @jsx MyReact.createElement */
+const MyReact = getMyReact();
+
+const MyTodos = [
+  "Implement React render",
+  "Implement React component",
+  "Implement React fiber",
+  "Replace original React",
+  "Conquer the world!"
+];
+
+const TEXT_ELEMENT = "TEXT_ELEMENT";
+
+const myElement = (
+  <div>
+    <ul>
+      <li>Implement React render</li>
+      <li>Implement React component</li>
+      <li>Implement React fiber</li>
+      <li>Replace original React</li>
+      <li>Conquer the world!</li>
+    </ul>
+  </div>
+);
+
+MyReact.render(myElement, document.getElementById("my-react-app"));
+
+// Probably need to build everything into a single file.
+// For now just adding the source code of MyReact here since require() is not supported ny browsers
+function getMyReact() {
+  function render(element, parent) {
+    // Get the props and type from element object
+    const { type, props } = element;
+
+    // Check if it is a text element
+    const isTextElement = type === "TEXT_ELEMENT";
+
+    // Create a new dom element
+    const dom = !isTextElement
+      ? document.createElement(type)
+      : document.createTextNode(props.nodeValue);
+
+    // Filter for eventListeners in the props
+    const isListener = name => name.startsWith("on");
+
+    // Add eventListeners to the dom element
+    Object.keys(props)
+      .filter(isListener)
+      .forEach(name => {
+        const eventType = name.toLowerCase().substring(2);
+        dom.addEventListener(eventType, props[name]);
+      });
+
+    const isAttribute = name => !isAttribute && name !== "children";
+
+    Object.keys(props)
+      .filter(isAttribute)
+      .forEach(name => {
+        dom[name] = props[name];
+      });
+
+    // Check if there are any childrens of the given element
+    const childElements = props.children || [];
+
+    // render those childrens recursively first
+    childElements.forEach(el => render(el, dom));
+
+    // finally append the element to the parent element
+    parent.appendChild(dom);
+  }
+
+  function createElement(type, config, ...args) {
+    const props = Object.assign({}, config);
+    const hasChildren = args.length > 0;
+
+    const allChildren = hasChildren ? [].concat(...args) : [];
+
+    props.children = allChildren
+      .filter(child => child !== null && child !== false)
+      .map(child =>
+        child instanceof Object ? child : createTextElement(child)
+      );
+
+    return { type, props };
+  }
+
+  function createTextElement(value) {
+    return {
+      type: TEXT_ELEMENT,
+      props: {
+        nodeValue: value
+      }
+    };
+  }
+  return {
+    render,
+    createElement
+  };
+}
